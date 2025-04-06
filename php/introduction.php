3c2dcc1e18ca64012407770695bd6944030dbc4e
Brenda Sayonara <149035540+bsouzao0@users.noreply.github.com>
2025-04-05 21:55:27 -0400

    Update introduction.php

--- php/introduction.php
+++ php/introduction.php
@@ -15,28 +15,28 @@
             <figcaption>Coffee time to survive this life</figcaption>
         </figure>
         <ul>
-            <li><b>Personal background:</b>  Born and raised in the north of Brazil, I moved to the US in 2020. </li>
-            <li><b>Professional background:</b>  In my country, I worked as an English teacher in high school, as a computer 
+            <li><strong>Personal background:</strong>  Born and raised in the north of Brazil, I moved to the US in 2020. </li>
+            <li><strong>Professional background:</strong>  In my country, I worked as an English teacher in high school, as a computer 
             instructor (Microsoft Office), and as a coordinator for a social assistance reference center. Last Fall semester 
             2024 I worked as a Service Desk Intern at CPCC.</li>
-            <li><b>Academic background:</b>  I graduated in Brazil with a degree in English Literature, and now I am pursuing 
+            <li><strong>Academic background:</strong>  I graduated in Brazil with a degree in English Literature, and now I am pursuing 
             Information Technology: Full Stack Programming and Cloud &amp; Virtualization Tech.</li>
-            <li><b>Background in this subject:</b>  I took Web 110, CTI110, and Web 115</li>
-            <li><b>Primary Computer Platform:</b>  MacBook Air M1, 2020</li>
-            <li><b>Courses I'm Taking &amp; Why:</b> 
+            <li><strong>Background in this subject:</strong>  I took Web 110, CTI110, and Web 115</li>
+            <li><strong>Primary Computer Platform:</strong>  MacBook Air M1, 2020</li>
+            <li><strong>Courses I'm Taking &amp; Why:</strong> 
                 <ul>
-                    <li><b>CTS240 – Project Management:</b> Most of my courses this semester are essential for completing my AAS - Full Stack Programming.</li>
-                    <li><b>CSC154 – Software Development:</b>  I am taking this class now because I waited until the last minute to do it and now it is important to graduate in my course this semester.
+                    <li><strong>CTS240 – Project Management:</strong> Most of my courses this semester are essential for completing my AAS - Full Stack Programming.</li>
+                    <li><strong>CSC154 – Software Development:</strong>  I am taking this class now because I waited until the last minute to do it and now it is important to graduate in my course this semester.
                     </li>
-                    <li><b>NET126 – Switching and Routing:</b>  It is part of my major in Cloud &amp; Virtualization Tech, and also helps me with options for different career paths while I am also considering some Cisco certifications.
+                    <li><strong>NET126 – Switching and Routing:</strong>  It is part of my major in Cloud &amp; Virtualization Tech, and also helps me with options for different career paths while I am also considering some Cisco certifications.
                     </li>
-                    <li><b>WEB215 – Advanced Markup &amp; Scripting</b> I would like to become a Web Developer, so this class will help me to accomplish my goals.</li>
-                    <li><b>WEB250 – Database Driven Websites:</b> It is part of my Web Developer path.</li>
-                    <li><b>CTS – IS Professional Communication:</b> As a non-native English speaker, I think this class is essential to help me build vocabulary and learn to communicate in the corporate world.
+                    <li><strong>WEB215 – Advanced Markup &amp; Scripting</strong> I would like to become a Web Developer, so this class will help me to accomplish my goals.</li>
+                    <li><strong><a href="contract.html"></a>WEB250 – Database Driven Websites:</a></strong> It is part of my Web Developer path.</li>
+                    <li><strong>CTS – IS Professional Communication:</strong> As a non-native English speaker, I think this class is essential to help me build vocabulary and learn to communicate in the corporate world.
                     </li>
                 </ul>
             </li>
-            <li><b>Funny/Interesting Item to Remember me by:</b> I like to read Mangas and watch Animes.</li>
+            <li><strong>Funny/Interesting Item to Remember me by:</strong> I like to read Mangas and watch Anime.</li>
         </ul>
     </main>
 
