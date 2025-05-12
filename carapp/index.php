158853c64266e53e1eccf5a4ad2f31cba70b0ee1
Brenda Sayonara <149035540+bsouzao0@users.noreply.github.com>
2025-05-11 23:00:42 -0400

    s

--- carapp/index.php
+++ carapp/index.php
@@ -12,7 +12,7 @@ $cars = getCars($mysqli);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/x-icon" href="../images/blueorchid.png?">>
+    <link rel="shortcut icon" type="image/x-icon" href="../images/blueorchid.png?">
     <link rel="stylesheet" href="styles/default.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
